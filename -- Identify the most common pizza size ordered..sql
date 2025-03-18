549bc05e3c6015be5fb46757ca432cd9885e5c3f
Devanshu Sharma <devanshusharma.mhs@gmail.com>
2025-03-18 23:05:52 +0530

    Add files via upload

---
+++ -- Identify the most common pizza size ordered..sql
@@ -0,0 +1,21 @@
+-- count a quantity 
+
+SELECT 
+    quantity, COUNT(order_details_id)
+FROM
+    orders_details
+GROUP BY quantity;
+
+
+
+-- Identify the most common pizza size ordered.
+
+SELECT 
+    pizzas.size,
+    COUNT(orders_details.order_details_id) AS order_count
+FROM
+    pizzas
+        JOIN
+    orders_details ON pizzas.pizza_id = orders_details.pizza_id
+GROUP BY pizzas.size
+ORDER BY order_count DESC;
